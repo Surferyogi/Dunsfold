504dd3efa31d87fbdc5e2a7f225519aaff18fd6d
Surferyogi <koksum@yahoo.com>
2026-07-15 11:59:44 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "dunsfold-reno-v2";
+const CACHE = "dunsfold-reno-v4";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
